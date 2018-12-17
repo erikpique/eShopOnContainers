8c9524a913d2f3b8e219a5e9a5c90691e6193c3f
Erik Pique <epique@plainconcepts.com>
2018-12-17 10:41:56 +0100

    webhook init

---
+++ src/Services/WebHook/WebHook.API/Models/WebHookSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace WebHook.API.Models
+{
+    public class WebHookSettings
+    {
+        public bool AzureServiceBusEnabled { get; set; }
+
+        public string EventBusConnection { get; set; }
+
+        public string EventBusUserName { get; set; }
+
+        public string EventBusPassword { get; set; }
+
+        public int EventBusRetryCount { get; set; }
+
+        public string SubscriptionClientName { get; set; }
+    }
+}
---
+++ src/Services/WebHook/WebHook.API/ServiceCollectionExtensions/AutoFactExtension.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace WebHook.API.ServiceCollectionExtensions
+{
+    public static class AutoFactExtension
+    {
+        public static IServiceProvider AddContainer(this IServiceCollection services)
+        {
+            var container = new ContainerBuilder();
+
+            container.Populate(services);
+
+            return new AutofacServiceProvider(container.Build());
+        }
+    }
+}
